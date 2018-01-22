bf6efd12c0e115057a0e479b027b1fd492357d28
Your Name <742862703@qq.com>
2018-01-22 12:43:20 +0000

    Finish static pages

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+@section('title','aboutpage')
+
+@section('content')
+<h1>aboutpage</h1>
+@stop
---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.default')
+@section('content')
+<h1>helppage</h1>
+@stop
